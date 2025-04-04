e870655a0be80748cb33a4d24e0bd0d056f749e1
Damian Krawczyk <pirrat82@gmail.com>
2025-04-04 16:42:21 +0200

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-export const Person = ({ person }) => {
-  const { name, age, sex, isMarried, partnerName } = person;
-
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => {
   const getPartnerText = () => {
     if (!isMarried) return 'I am not married';
-
     const partnerRole = sex === 'm' ? 'wife' : 'husband';
 
     return `${partnerName} is my ${partnerRole}`;
@@ -14,7 +13,7 @@ export const Person = ({ person }) => {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
+      {age && <p className="Person__age">I am {age} years old</p>}
       <p className="Person__partner">{getPartnerText()}</p>
     </section>
   );
