431d1bc51d5a157ab5b1c8d0e4e27ba8e60c2b1e
Damian Krawczyk <pirrat82@gmail.com>
2025-04-04 16:51:55 +0200

    task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -13,7 +13,7 @@ export const Person = ({
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age} years old</p>}
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{getPartnerText()}</p>
     </section>
   );
